2d48f09d7fa542ed334bdadb02ad657bc956096d
florinda zeida <zeidaflorinda@gmail.com>
2020-03-11 12:03:17 +0100

    script_1

---
+++ script.js
@@ -0,0 +1,63 @@
+// question 1
+var petArray = ["cat", "cow", "dog"];
+
+// question 2
+var petArray = ["cat", "cow", "dog"];
+console.log(petArray[1]);
+// cow
+
+// question 3
+var petArray = ["cat", "cow", "dog"];
+petArray.push("sheep");
+console.log(petArray);
+// [ 'cat', 'cow', 'dog', 'sheep' ]
+
+// question 4
+var petArray = ["cat", "cow", "dog"];
+petArray.push("sheep");
+console.log(petArray.length);
+// 4
+
+// question 5
+var catObject = {
+    name: "Tasy",
+    colour: "gray",
+    age: "9"
+};
+
+// question 6
+var catObject = {
+    name: "Tasy",
+    colour: "gray",
+    age: "9"
+};
+console.log(catObject.age);
+// 9
+
+// question 7
+var catArray = [{
+    name: "Tasy",
+    colour: "gray",
+    age: "9"
+}];
+
+// question 8
+var catArray = [{
+    name: "Tasy",
+    colour: "gray",
+    age: "9"
+}];
+console.log(catArray);
+// [ { name: 'Tasy', colour: 'gray', age: '9' } ]
+
+// question 9
+function logToConsole(myTree) {
+    console.log("myTree");
+}
+
+// question 10
+function logToConsole(myTree) {
+    console.log(myTree);
+}
+logToConsole("donkey");
+// donkey
